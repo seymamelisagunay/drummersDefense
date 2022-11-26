92ed713fc9d2830080afeedb30579ec2ad328830
Ramazan Arda Asci <rardaasci@hotmail.com>
2022-11-26 12:43:46 +0300

    Spawn System

--- Assets/Arda/Scripts/Spawn.cs
+++ Assets/Arda/Scripts/Spawn.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<GameObject> Enemies;
+    public List<int> MaxSpawnCount;
+    public List<Vector2> Waves;
+    public int waveId;
+    public bool IsWaveStarted;
+    private float nextSpawnTime = 0;
+    private float nextSpawnTime1 = 0;
+    private int spawnerCounter1;
+    private int spawnerCounter2;
+    [SerializeField] private float spawnPeriod = 2f;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        for (int i = 0; i < Waves.Count; i++)
+        {
+            if (IsWaveStarted)
+            {
+                if (spawnerCounter2 != Waves[i].y)
+                {
+                    if (Time.time >= nextSpawnTime)
+                    {
+                        nextSpawnTime = Time.time + spawnPeriod;
+                        Enemy2Spawner(Waves[i].y);
+                    }
+                }
+
+
+
+                if (spawnerCounter1 !=Waves[i].x)
+                {
+                    if (Time.time >= nextSpawnTime1)
+                    {
+                        nextSpawnTime1 = Time.time + spawnPeriod;
+                        Enemy1Spawner(Waves[i].x);
+                    }
+                }
+            }
+
+        }     
+
+    }
+
+    public void Enemy1Spawner(float x)
+    {
+        if (x>0)
+        {
+            Instantiate(Enemies[0]);
+            spawnerCounter1++;
+        }
+       
+    }
+    public void Enemy2Spawner(float y)
+    {
+        if (y > 0)
+        {
+            Instantiate(Enemies[1]);
+            spawnerCounter2++;
+        }
+
     }
 }
